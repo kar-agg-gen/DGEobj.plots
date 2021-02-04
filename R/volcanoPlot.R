bd8e35bad541482cfde9f92897b024fe999cebd1
kar-agg-gen <kartikeya@aggregate-genius.com>
2021-02-04 09:46:16 -0800

    Updating data processing logic

--- R/volcanoPlot.R
+++ R/volcanoPlot.R
@@ -179,22 +179,9 @@ volcanoPlot <- function(contrastDF,
     y <- "NegativeLogP"
 
     contrastDF$group <- NA
-    for (i in seq(nrow(contrastDF))) {
-        if (!is.na(contrastDF[i, pvalCol])) {
-            if (contrastDF[i, pvalCol] <= pthreshold) {
-                if (!is.na(contrastDF[i, logRatioCol])) {
-                    if (contrastDF[i, logRatioCol] > 0) {
-                        contrastDF$group[i] <- "Increased"
-                    } else if (contrastDF[i, logRatioCol] < 0) {
-                        contrastDF$group[i] <- "Decreased"
-                    }
-                }
-            } else {
-                contrastDF$group[i] <- "No Change"
-            }
-        }
-    }
-
+    contrastDF$group <- ifelse(contrastDF[[pvalCol]] <= pthreshold,
+                               ifelse(contrastDF[[logRatioCol]] >= 0, "Increased", "Decreased"),
+                               "No Change")
     contrastDF$group <- contrastDF$group %>%
         factor(levels = c("Increased", "Decreased", "No Change"))
 

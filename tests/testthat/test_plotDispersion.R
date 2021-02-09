8b4db63af8e5bfe299d6cb36e74819eae687f025
kartikeya kirar <49568503+kar-agg-gen@users.noreply.github.com>
2021-02-10 05:24:33 +0530

    Updating plotDispersion.R with cx plot functionality (#13)

--- tests/testthat/test_plotDispersion.R
+++ tests/testthat/test_plotDispersion.R
@@ -10,19 +10,33 @@ test_that("plotDispersion.R: plotDispersion()", {
 
     # testing with input DGEList
     plot_disp <- plotDispersion(DGEdata = dgelist, designMatrix = designMatrix)
+    expect_s3_class(plot_disp, c("canvasXpress", "htmlwidget"))
+
+    # testing with input DGEList
+    plot_disp <- plotDispersion(DGEdata = dgelist, designMatrix = designMatrix, plotType = "ggplot")
     expect_s3_class(plot_disp, c("gg", "ggplot"))
 
     # testing with input countMatrix
     plot_disp <- plotDispersion(DGEdata       = t_obj1$counts,
                                 designMatrix  = designMatrix,
                                 lineFit       = "loess",
-                                rugColor      = "red",
-                                plotType      = "BCV")
+                                plotCategory  = "BCV")
+    expect_s3_class(plot_disp, c("canvasXpress", "htmlwidget"))
+
+    plot_disp <- plotDispersion(DGEdata       = t_obj1$counts,
+                                plotType      = "ggplot",
+                                designMatrix  = designMatrix,
+                                lineFit       = "loess",
+                                plotCategory  = "BCV")
     expect_s3_class(plot_disp, c("gg", "ggplot"))
 
     # testing assert statements
     expect_error(plotDispersion(),
                  regexp = "DGEdata must be specified.")
     expect_error(plotDispersion(DGEdata = t_obj1$counts),
-                 regexp = "argument \"designMatrix\" is missing, with no default")
+                 regexp = "designMatrix must be specified and should be of class 'matrix'.")
+    expect_error(plotDispersion(DGEdata = dgelist, designMatrix = designMatrix, plotCategory = "cx"),
+                 regexp = "Plot value must be either dispersion or BCV.")
+    expect_error(plotDispersion(DGEdata = dgelist, designMatrix = designMatrix, plotType = "cx"),
+                 regexp = "Plot type must be either canvasXpress or ggplot.")
 })

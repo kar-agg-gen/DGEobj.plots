ff806a5a971ed3e6e91117bb600fe8fadbd8fa1a
kartikeya kirar <49568503+kar-agg-gen@users.noreply.github.com>
2021-02-10 05:22:41 +0530

    Updating mapDGEobj.R with cx plot functionality (#11)

--- tests/testthat/test_mapDGEobj.R
+++ tests/testthat/test_mapDGEobj.R
@@ -3,10 +3,13 @@ context("DGEobj.plots - tests for mapDGEobj.R functions")
 
 test_that('mapDGEobj.R: mapDGEobj()', {
     map_DGEobj <- mapDGEobj(t_obj1)
+    expect_s3_class(map_DGEobj, c("canvasXpress", "htmlwidget"))
+
+    map_DGEobj <- mapDGEobj(t_obj1, plotType = "ggplot")
     expect_s3_class(map_DGEobj, "igraph")
 
     expect_error(mapDGEobj(obj),
                  regexp = "object 'obj' not found")
-    expect_error(mapDGEobj("XYZ"),
-                 regexp = "dgeObj must be of class 'DGEobj'.")
+    expect_error(mapDGEobj(t_obj1, plotType = "cx"),
+                 regexp = "Plot type must be either canvasXpress or ggplot.")
 })
